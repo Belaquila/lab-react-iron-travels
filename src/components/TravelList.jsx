755af39993eeb9ce7208188de1e632c32987c454
ab.belabdia@gmail.com <ahmed@MacBook-Pro-de-Ahmed.local>
2024-10-23 22:32:03 +0200

    add delete button

--- src/components/TravelList.jsx
+++ src/components/TravelList.jsx
@@ -1,31 +1,44 @@
 import React, { useState } from "react";
 import travelPlansData from "../assets/travel-plans.json";
 
-function TravelList (){
+function TravelList() {
+  const [displayedPlans, setPlansToDisplay] = useState(travelPlansData);
 
-    return (
-        <section>
-            <div>
-                {travelPlansData.map((travelPlan, index)=>{
-                    let isGreatDeal = travelPlan.totalCost <= 350;
-                    let isPremium = travelPlan.totalCost >= 1500;
-                    return (
-                        <div className="travelPlan" key={travelPlan.id}>
-                            <img className="image" src={travelPlan.image}/>
-                            <div className="info">
-                                <p>{travelPlan.destination} ({travelPlan.days} days )</p>
-                                <p>{travelPlan.description}</p>
-                                <p>Price: {travelPlan.totalCost} €</p>
-                                {isGreatDeal && <p className="label">Great Deal</p>}
-                                {isPremium && <p className="label">Premium</p>}
-                            </div>
-                        </div>
-                    )
-                })}
+  const removeTravel = (id) => {
+    const newArr = displayedPlans.filter((travelPlan) => {
+      return travelPlan.id !== id;
+    });
+
+    setPlansToDisplay(newArr);
+  };
+
+  return (
+    <section>
+      <div>
+        {displayedPlans.map((travelPlan, index) => {
+          let isGreatDeal = travelPlan.totalCost <= 350;
+          let isPremium = travelPlan.totalCost >= 1500;
+          return (
+            <div className="travelPlan" key={travelPlan.id}>
+              <img className="image" src={travelPlan.image} />
+              <div className="info">
+                <p>
+                  {travelPlan.destination} ({travelPlan.days} days )
+                </p>
+                <p>{travelPlan.description}</p>
+                <p>Price: {travelPlan.totalCost} €</p>
+                {isGreatDeal && <p className="labelGD">Great Deal</p>}
+                {isPremium && <p className="labelPremium">Premium</p>}
+                <button onClick={() => removeTravel(travelPlan.id)}>
+                  Delete
+                </button>
+              </div>
             </div>
-        </section>
-        
-    )
+          );
+        })}
+      </div>
+    </section>
+  );
 }
 
 export default TravelList;
